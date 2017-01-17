b9cf340714ee3c22b223d98ce5fe0b15ceefa3da
redsool <redsool@gmail.com>
2017-01-17 21:03:14 +0300

    now it's working

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -11,12 +12,21 @@ app.use(methodOverride())
 const schemas = require('./mongoose')
 
 for (let i in schemas) {
-    restify.serve(router, schemas[i], {lean: false})
+    restify.serve(router, schemas[i])
 }
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use(router)
 
+
+
 app.listen(3001, () => {
-    console.log('Express server listening on port 3000')
+    console.log('Express server listening on port 3001')
 })
 
--- mongoose.js
+++ mongoose.js
@@ -1,35 +1,35 @@
 const mongoose = require('mongoose');
 const relationship = require("mongoose-relationship")
-const autopopulate = require("mongoose-autopopulate")
 
-mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://redsool:unix999@ds064188.mlab.com:64188/redsool');
 
 
 
-const answerSchema = mongoose.Schema({
-    text: String,
-    question: {type: mongoose.Schema.Types.ObjectId, ref: 'Question', childPath:'answers' }
+const poolSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    questions: [{type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+    devices: [{type: mongoose.Schema.Types.ObjectId, ref: 'Device', autopopulate: true }]
 });
 
 const questionSchema = mongoose.Schema({
-    text: String,
+    title: String,
     type: String,
-    answers: [{type: mongoose.Schema.Types.ObjectId, ref: 'Answer', autopopulate: true }],
-    pool: {type: mongoose.Schema.Types.ObjectId, ref: 'Pool', childPath:'questions'}
+    answers: [{type: mongoose.Schema.Types.ObjectId, ref: 'Answer' }],
+    pool: { type: mongoose.Schema.Types.ObjectId, ref: 'Pool', childPath: 'questions'}
 });
 
-const poolSchema = mongoose.Schema({
-    text: String,
-    description: String,
-    questions: [{type: mongoose.Schema.Types.ObjectId, ref: 'Question', autopopulate: true }],
-    devices: [{type: mongoose.Schema.Types.ObjectId, ref: 'Device', childPath: 'pool'}]
+const answerSchema = mongoose.Schema({
+    title: String,
+    question: {type: mongoose.Schema.Types.ObjectId, ref: 'Question', childPath:'answers' }
 });
+
 
 const deviceSchema = mongoose.Schema({
     date: { type: Date, default: Date.now },
     token: String,
-    pool: { type: mongoose.Schema.Types.ObjectId, ref: 'Pool'}
+    pool: { type: mongoose.Schema.Types.ObjectId, ref: 'Pool', childPath: 'devices'}
 });
 
 const resultSchema = mongoose.Schema({
@@ -39,14 +39,9 @@ const resultSchema = mongoose.Schema({
 });
 
 
-answerSchema.plugin(autopopulate);
-questionSchema.plugin(autopopulate);
-poolSchema.plugin(autopopulate);
-
-
 answerSchema.plugin(relationship, {relationshipPathName:'question', triggerMiddleware: true});
 questionSchema.plugin(relationship, {relationshipPathName:'pool', triggerMiddleware: true});
-poolSchema.plugin(relationship, {relationshipPathName:'devices', triggerMiddleware: true});
+deviceSchema.plugin(relationship, {relationshipPathName:'pool', triggerMiddleware: true});
 
 
 const Answer = mongoose.model('Answer', answerSchema);
